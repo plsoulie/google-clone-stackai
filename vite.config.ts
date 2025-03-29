fefb43c9901f55c03869ad38d5cd319ba5b296b3
Pierre-Louis Soulié <167431889+plsoulie@users.noreply.github.com>
2025-03-29 15:26:21 -0700

    component tagger

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-
 import { defineConfig } from "vite";
 import path from "path";
+import { lovableTagger } from 'lovable-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [lovableTagger()],
   server: {
     host: "::",
     port: 8080,
